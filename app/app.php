05ba5653b4dfd88d290513e37b2209a2c9205b42
Erica Wright <ericaw21@gmail.com>
2017-02-14 23:38:35 -0800

    remove ping pong from title-case practice

--- app/app.php
+++ app/app.php
@@ -1,31 +1,24 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Game.php";
-
-    session_start();
-
-    if (empty($_SESSION['game_object'])) {
-      $_SESSION['game_object'] = array();
-    }
+    require_once __DIR__."/../src/TitleCaseGenerator.php";
 
     $app = new Silex\Application();
-
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
+
         return $app['twig']->render('form.html.twig');
     });
 
-    $app->get("/display", function() use ($app) {
-        $newGame = new Game($_GET['count_input']);
-        $newGame->save();
-        $count = $newGame->countTo($_GET['count_input']);
-        return $app['twig']->render('ping-pong-display.html.twig', array('numbers' =>$count, 'game_info' => Game::getAll(), 'thisgame' => $newGame ));
+    $app->get("/view_title_case", function() use($app) {
+        $my_TitleCaseGenerator = new TitleCaseGenerator;
+        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+        
+        return $app['twig']->render('title_cased.html.twig', array('result' => $title_cased_phrase));
     });
-
     return $app;
 ?>
